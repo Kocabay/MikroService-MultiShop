6d7c9648c09da5ee8f1a0d233e766990f41d4046
Ömer Kocabay <passanger54@hotmail.com>
2025-11-20 22:31:44 +0300

    Mediator

---
+++ Services/Order/Core/MultiShop.Order.Application/Features/Mediator/Handlers/OrderingHandlers/GetOrderingByQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using MultiShop.Order.Application.Features.Mediator.Queries.OrderingQueries;
+using MultiShop.Order.Application.Features.Mediator.Results.OrderingResults;
+using MultiShop.Order.Application.Interfaces;
+using MultiShop.Order.Domain.Entities;
+
+namespace MultiShop.Order.Application.Features.Mediator.Handlers.OrderingHandlers
+{
+    public class GetOrderingByQueryHandler : IRequestHandler<GetOrderingByIdQuery, GetOrderingByIdQueryResult>
+    {
+        private readonly IRepository<Ordering> _repository;
+
+        public GetOrderingByQueryHandler(IRepository<Ordering> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<GetOrderingByIdQueryResult> Handle(GetOrderingByIdQuery request, CancellationToken cancellationToken)
+        {
+            var values = await _repository.GetByIdAsync(request.Id);
+            return new GetOrderingByIdQueryResult
+            {
+                OrderDate = values.OrderDate,
+                TotalPrice = values.TotalPrice,
+                UserId = values.UserId,
+                OrderingId = values.OrderingId
+            };
+        }
+    }
+}
